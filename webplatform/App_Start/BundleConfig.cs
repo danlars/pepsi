43e1e2847baf02eb370190687493e3d692f30f68
dansker <daniellarsen725@hotmail.com>
2016-01-29 12:56:00 +0100

    fancybox

--- webplatform/App_Start/BundleConfig.cs
+++ webplatform/App_Start/BundleConfig.cs
@@ -17,6 +17,9 @@ namespace webplatform
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-fancybox").Include(
+                        "~/Scripts/jquery.fancybox*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -28,7 +31,8 @@ namespace webplatform
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery.validate*"));
         }
     }
 }
---
+++ webplatform/Controllers/FormController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webplatform.Models;
+
+namespace webplatform.Controllers
+{
+    public class FormController : Controller
+    {
+
+        // GET: Form/Card/{id}
+        [HttpGet]
+        public ActionResult Card(int id = 0)
+        {
+            if (id > 0)
+            {
+                TestModelBuilder card = new TestModelBuilder();
+                return View(card.getCards().Where(x => x.id == id));
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET: Form/Job/{id}
+        public ActionResult Job(int id = 0)
+        {
+            if (id > 0)
+            {
+                TestModelBuilder job = new TestModelBuilder();
+                return View(job.getJobs().Where(x => x.id == id));
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
